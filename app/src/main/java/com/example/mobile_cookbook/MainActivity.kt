b8e158e423f92d946787ecf88e787bc2c32f2373
pawel <pawel@gmail.com>
2025-03-14 11:22:16 +0100

    rating bar

--- app/src/main/java/com/example/mobile_cookbook/MainActivity.kt
+++ app/src/main/java/com/example/mobile_cookbook/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.mobile_cookbook
 
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ratingBar: RatingBar
+    private lateinit var ratingValue: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        ratingBar = findViewById(R.id.my_ratingbar)
+        ratingValue = findViewById(R.id.rating_value_textview)
+
+        val sharedPreferences = getSharedPreferences("MojePreferencje", MODE_PRIVATE)
+        val saveRatingValue = sharedPreferences.getFloat("ratingXYZ", 3.0f)
+
+        ratingBar.rating = saveRatingValue
+        ratingValue.text = "Twoja ocena $saveRatingValue"
+
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            ratingValue.text = "Twoja ocena: $rating"
+
+            val editor = sharedPreferences.edit()
+            editor.putFloat("ratingXYZ", rating)
+            editor.apply()
         }
     }
 }
